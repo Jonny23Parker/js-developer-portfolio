4423a7bc0c129b63c8f792f401f77fd169ccf9b7
Jonny23Parker <101059784+Jonny23Parker@users.noreply.github.com>
2023-04-10 19:05:13 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Jonny23Parker/js-developer-portfolio/main/data/Jonnyprofile.json'; 
+    const url = 'https://raw.githubusercontent.com/Jonny23Parker/js-developer-portfolio/main/data/Jonnyprofile.json'; sercontent.com/digit
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
